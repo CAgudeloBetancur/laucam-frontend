63e393c8797acc655e2230e9b2e76ef09936d0e1
Camilo_AB <camiloab97@gmail.com>
2024-04-12 13:19:17 -0500

    Refactorizacion elementsTable, creacion modalForm

--- src/App.js
+++ src/App.js
@@ -1,117 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import MovieDetail from "./Components/MovieDetail.jsx";
 import ElementsTable from "./Components/ElementsTable.jsx";
 import Navbar from "./Components/Navbar.jsx";
 import Footer from "./Components/Footer.jsx";
 
-const columnasMedia = ["serial", "titulo", "urlPelicula"];
-const columnasGenero = ["nombre", "estado", "descripcion"];
-const columnasDirector = ["nombre", "estado"];
-const columnasProductora = ["nombre", "slogan", "descripcion"];
-const columnasTipo = ["nombre", "descripcion"];
+import { 
+  formInputsDirector, 
+  formInputsGenero, 
+  formInputsMedia, 
+  formInputsProductora, 
+  formInputsTipo 
+  } from "./utils/inputsSchemas.js";
 
-const formInputsMedia = [
-  {
-    name: 'serial',
-    type: 'text'
-  },
-  {
-    name: 'titulo',
-    type: 'text ',
-  },
-  {
-    name: 'sinopsis',
-    type: 'textarea'
-  },
-  {
-    name: 'urlPelicula',
-    type: 'text'
-  },
-  {
-    name: 'imagen',
-    type: 'text'
-  },
-  {
-    name: 'fechaEstreno',
-    type: 'date'
-  },
-  {
-    name: 'generoPrincipal',
-    model: 'genero',
-    type: 'select'
-  },
-  {
-    name: 'directorPrincipal',
-    model: 'director',
-    type: 'select'
-  },
-  {
-    name: 'productora',
-    model: 'productora',
-    type: 'select'
-  },
-  {
-    name: 'tipo',
-    model: 'tipo',
-    type: 'select'
-  }
-]
-
-const formInputsDirector = [
-  {
-    name: 'nombre',
-    type: 'text'
-  },
-  {
-    name: 'estado',
-    type: 'select'
-  }
-]
-
-const formInputsGenero = [
-  {
-    name: 'nombre',
-    type: 'text'
-  },
-  {
-    name: 'estado',
-    type: 'select'
-  },
-  {
-    name: 'descripcion',
-    type: 'textarea'
-  }
-]
-
-const formInputsProductora = [
-  {
-    name: 'nombre',
-    type: 'text'
-  },
-  {
-    name: 'estado',
-    type: 'select'
-  },
-  {
-    name: 'slogan',
-    type: 'text'
-  },
-  {
-    name: 'descripcion',
-    type: 'textarea'
-  }
-]
-
-const formInputsTipo = [
-  {
-    name: 'nombre',
-    type: 'text'
-  },
-  {
-    name: 'descripcion',
-    type: 'textarea'
-  },
-]
+import { 
+  columnasDirector, 
+  columnasGenero, 
+  columnasMedia, 
+  columnasProductora, 
+  columnasTipo 
+  } from "./utils/columnasTabla.js";
 
 function App() {
   return (
@@ -125,7 +33,6 @@ function App() {
             <Route path="/tipo" element={<ElementsTable modelo="tipo" columnas={columnasTipo} formInputs={formInputsTipo}/>}/>
             <Route path="/productora" element={<ElementsTable modelo="productora" columnas={columnasProductora} formInputs={formInputsProductora}/>}/>
             <Route path="/genero" element={<ElementsTable modelo="genero" columnas={columnasGenero} formInputs={formInputsGenero}/>}/>
-            {/* <Route path="/" element={<ShowMovies/>}/> */}
             <Route path="/media/:id" element={<MovieDetail modelo="media"/>}/>
           </Routes>
         </div>
--- src/Components/ElementsTable.jsx
+++ src/Components/ElementsTable.jsx
@@ -5,6 +5,7 @@ import { useDropzone } from 'react-dropzone';
 import axios from 'axios';
 import withReactContent from 'sweetalert2-react-content';
 import Swal from 'sweetalert2';
+import ModalForm from './ModalForm.jsx';
 
 const capitalizarString = (str) => `${str.charAt(0).toUpperCase()}${str.slice(1)}`
 
@@ -351,113 +352,20 @@ const ElementsTable = ({modelo, columnas, formInputs}) => {
 
       </div>
 
-      <div id='modalMovies' className='modal fade' area-hidden='true' onClick={(e) => {cancelar(e)}}>
-        <div className='modal-dialog'>
-          <div className="modal-content">
-            <div className='modal-header'>
-              <span className='h5'>{tituloModal}</span>
-              <button 
-                id='btnCerrarX'
-                onClick={(e) => {cancelar(e)}}
-                type='button' 
-                className='btn-close' 
-                data-bs-dismiss='modal'
-                aria-label='Close'>
-              </button>
-            </div>
-            <div className='modal-body'>
-              <input type="hidden" id="id"/>
-              { 
-                formInputs.map((input,i) => {
-                  return (
-                    <div key={i}>
-                      <div className={`input-group ${errors[input.name]?.length > 0 ? 'mb-0' : 'mb-3'}`}>
-                        <span className='input-group-text'>
-                          {input.name}
-                        </span>
-                        {
-                          (input.type === 'select') && 
-                            <select className={`form-select ${errors[input.name]?.length > 0 ? 'is-invalid' : ''}`} name={input.name} value={campos[input.name]} onChange={capturarCampos}>
-                                <option value={'default'} >-- Selecciona --</option>
-                                {
-                                  (input.name === 'estado')
-                                  ? <>
-                                      <option value='Activo'>Activo</option>
-                                      <option value='Inactivo'>Inactivo</option>
-                                    </>
-                                  : 
-                                    options[input.model]?.map((option,i) => {
-                                      return (
-                                        <option key={i} value={option.value}>{option.label}</option>
-                                      )
-                                    })
-                                }
-                              </select>                        
-                          
-                        }
-                        {
-                          (input.type === 'textarea') && 
-                            <textarea name={input.name} id={input.name} className={`form-control ${errors[input.name]?.length > 0 ? 'is-invalid' : ''}`} value={campos[input.name]} onChange={capturarCampos} placeholder={input.name}></textarea>
-                        }                    
-                        {
-                          (input.type !== 'textarea' && input.type !== 'select' && input.type !== 'file') &&
-                            <input 
-                              type={[input.type]}
-                              name={input.name}
-                              id={input.name}
-                              className={`form-control ${errors[input.name]?.length > 0 ? 'is-invalid' : ''}`}
-                              placeholder={input.name}
-                              value={campos[input.name]}
-                              onChange={capturarCampos}/>
-                        }
-                        
-                      </div>
-                      <div>
-                        {
-                          (errors[input.name] !== undefined && errors[input.name]?.length > 0) && 
-                            errors[input.name]?.map((error, i) => {
-                              return(
-                                <p key={i} className='text-danger mb-3'>{error}</p>
-                              )
-                          })
-                        }
-                      </div>
-                      {
-                        (previsualizacionUrl && input.name === 'imagen') && 
-                        <div className='mt-3 mb-3'>
-                          <div className="d-flex align-items-center justify-content-center">
-                            <img className='d-inline-block' src={previsualizacionUrl} alt="" style={{ 'width' : '250px' }}/>
-                          </div>
-                        </div>
-                      }
-                    </div>
-                  )
-                })
-              }
-              <div className='d-grid col-6 mx-auto'>
-                <button 
-                  disabled={cargando}
-                  className='btn btn-success'
-                  onClick={() => validarCampos()}>
-                  <i className='fa-solid fa-floppy-disk'></i>
-                  &nbsp;
-                  Guardar
-                </button>
-              </div>
-            </div>
-            <div className='modal-footer'>
-              <button 
-                id="btnCerrar"
-                type='button'
-                className='btn btn-secondary'
-                onClick={(e) => cancelar(e)}
-                data-bs-dismiss='modal'>
-                Cancelar
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      {/* Formulario Modal */}
+
+      <ModalForm
+        cancelar = {cancelar}
+        tituloModal = {tituloModal}
+        formInputs = {formInputs}
+        errors = {errors}
+        options = {options}
+        campos = {campos}
+        previsualizacionUrl = {previsualizacionUrl}
+        cargando = {cargando}
+        validarCampos = {validarCampos}
+        capturarCampos = {capturarCampos}
+      />
 
     </div>
   )
